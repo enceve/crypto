e757635d85d7797a8ae575d899cc504a3bbca33d
EncEve <enceve@mail.de>
2016-05-24 21:21:32 +0200

    SIMD optimisation for blake2b - See #7
    blake2b core function uses 128bit XMM registers for the amd64 arch.

--- blake2b/blake2b_ref.go
+++ blake2b/blake2b_ref.go
@@ -1,6 +1,8 @@
 // Use of this source code is governed by a license
 // that can be found in the LICENSE file.
 
+// +build !amd64, appengine, gccgo
+
 package blake2b
 
 // the core blake2b function taking:
